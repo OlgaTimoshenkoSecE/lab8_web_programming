ff55f5e5633335157ba5dcdeeb25ada9f2b67df9
OlgaTimoshenkoSecE <n01679143@humber.ca>
2024-10-18 02:19:44 -0400

    part 3
    final touches

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # lab8_web_programming
  1) Found API for wizards from Harry Potter book series and decided to make an interactive page which will show the character's birthday, full name, hogwarts house, image and the actor who played it. 
  2) Tried to fetch the data, then specified what king of data (for which character) i need based on the user's input.
- 3) Started doing the layout for the page in HTML & CSS. 
+ 3) Started doing the layout for the page in HTML & CSS.
+ 4) Linked object attributes to correspondive blocks on the page
+ List of characters uncluded in the API:
+    Harry; Hermione; Ron; Fred; George; 
+    Bill; Percy; Charlie; Ginny; Molly; 
+    Arthur; Neville; Luna; Draco; Dumbledore; 
+    Minerva; Lupin; Hagrid; Sirius; Snape;
+    Bella; Voldemort; Cedric; Tonks; James
+
+
--- index.js
+++ index.js
@@ -1,33 +1,61 @@
 
 
-
-
-
-
-
-
 async function fetchWizard() {
     try{
-        let wizardName = document.getElementById("wizardName").value;
+        let wizardName = document.getElementById("wizardName").value; //getting input from the user
+
+        // if the input is not blank, make the nickname first letter uppercase (because thats how they are mentioned in API objects)
         if (wizardName!=undefined){
             wizardName = wizardName[0].toUpperCase() + wizardName.slice(1);
         } 
 
-        const result = await fetch("https://potterapi-fedeperin.vercel.app/en/characters");
-        const wizardInfo = await result.json();
+        const result = await fetch("https://potterapi-fedeperin.vercel.app/en/characters"); //fetch API
+        const wizardInfo = await result.json(); //convert into JSON
 
-        for (let i=0; i<25; i++){
+        /*following if loop iterates over each object(wizard) and 
+        if the nickname which the user typed in corresponds to any of them, display
+        some info about this wizard */
+
+        for (let i=0; i<25; i++){ 
             let wizard = wizardInfo[i];
             if (wizard.nickname==wizardName){
-                console.log(wizard);
+
+                document.getElementById("template").style.display = "grid"; //show the block where everyhting will be displayed
+
+                document.getElementById("item-1").textContent=`Full name: ${wizard.fullName}`;
+                document.getElementById("item-2").textContent=`Date of birth: ${wizard.birthdate}`;
+                document.getElementById("item-4").textContent=`Hogwarts House: ${wizard.hogwartsHouse}`;
+                document.getElementById("item-5").textContent=`Actor: ${wizard.interpretedBy}`;
+
+                const wizardImage = document.getElementById("wizardImage");
+                wizardImage.src = wizard.image;
+                wizardImage.style.display = "block";
+
+                //a little style change
+
+                if (wizard.hogwartsHouse == "Gryffindor"){
+                    document.getElementById("template").style.background = "darkred";
+                }
+                if (wizard.hogwartsHouse == "Ravenclaw"){
+                    document.getElementById("template").style.background = "darkblue";
+                }
+                if (wizard.hogwartsHouse == "Slytherin"){
+                    document.getElementById("template").style.background = "darkgreen";
+                }
+                if (wizard.hogwartsHouse == "Hufflepuff"){
+                    document.getElementById("template").style.background = "darkgoldenrod";
+                }
             };
         }
+
+        //clear input field
         document.getElementById("wizardName").value = "";
     }
-    catch(error) {
+    catch(error) { //error handling
         console.error(error);
     }
 }
+
 
 // functions for an input field
 function focusFunction(event){
